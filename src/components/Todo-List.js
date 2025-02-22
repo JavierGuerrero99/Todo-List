f227ea879f5754c5eec13c3e18bedfe397fa325b
WilliamRincon02 <rinconwilliam996@gmail.com>
2025-02-22 12:10:59 -0500

    Delete fixed and the delete button has been adjusted

--- src/components/Todo-List.js
+++ src/components/Todo-List.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const API_URL = "https://todolist-django-2nb4.onrender.com/api/tasks";
+const API_URL = "https://todolist-django-2nb4.onrender.com/api/tasks/";
 
 const TodoList = () => {
   const [tasks, setTasks] = useState([]);
@@ -47,8 +47,9 @@ const TodoList = () => {
   // Eliminar una tarea existente
   const deleteTask = async (id) => {
     try {
-      await axios.delete(`${API_URL}/${id}`);
+      await axios.delete(`${API_URL}${id}/`);
       setTasks(tasks.filter((task) => task.id !== id));
+      console.log(id)
     } catch (error) {
       console.error("Error al eliminar tarea:", error);
     }
